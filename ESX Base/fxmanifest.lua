127b91808594980ca279445b833bd5339a90e666
Abel Gaming <lcpdpolice4939@gmail.com>
2021-05-28 00:16:11 -0400

    Create fxmanifest.lua

---
+++ ESX Base/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game 'gta5'
+description 'Script description'
+author 'Author Name'
+version '1.0'
+
+server_scripts {
+	'server/main.lua'
+}
+
+client_scripts {
+	'client/main.lua'
+}
