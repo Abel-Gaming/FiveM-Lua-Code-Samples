78159f13db2a89898c4d22841792ff639beb091d
Abel Gaming <lcpdpolice4939@gmail.com>
2022-05-18 01:15:15 -0400

    Update main.lua

--- ESX Base/client/main.lua
+++ ESX Base/client/main.lua
@@ -1,5 +1,6 @@
-ESX              = nil
+ESX = nil
 
+-- Always make sure that this thread is on the top
 Citizen.CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
