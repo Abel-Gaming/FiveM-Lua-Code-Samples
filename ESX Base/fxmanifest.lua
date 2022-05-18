02136f90dc56c86e800b3037fd77de97ee9539e3
Abel Gaming <lcpdpolice4939@gmail.com>
2022-05-18 01:14:37 -0400

    Update fxmanifest.lua

--- ESX Base/fxmanifest.lua
+++ ESX Base/fxmanifest.lua
@@ -5,11 +5,11 @@ author 'Author Name'
 version '1.0'
 
 server_scripts {
-	--'config.lua',
+	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
-	--'config.lua',
+	'config.lua',
 	'client/main.lua'
 }
